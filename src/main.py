79d489861f5f3be694f303ec21bcdefe92c5fb9b
Jacek Miszczak <jacekmiszczak@gmail.com>
2014-05-22 23:05:58 +0200

    Basic CLI implementation

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
+"""
+Python MLA
+
+Usage:
+  main.py [<task_file>] [-v]
+  main.py -h | --help
+
+Options:
+  -h --help     Show this screen.
+  -v --verbose  Print more information on screen
+"""
+
 from model_reader import read_model
 from task_reader import read_task
 from solver import Solver
 from config import NOT, AND, OR
+from docopt import docopt
 
 def format_fact(final_set):
     return (OR+"\n").join(
@@ -20,24 +33,31 @@ def format_fact(final_set):
 TASK_PATH = 'example_input.yml'
 
 if __name__ == '__main__':
+    arguments = docopt(__doc__)
+    if arguments['<task_file>']:
+        TASK_PATH = arguments['<task_file>']
+
     model = read_model()
     task = read_task(TASK_PATH, model)
     solver = Solver(model)
     s1, s2, s = solver.solve(task)
 
-    print("S1")
-    for _s in sorted(list(s1)):
-        print(_s)
-    print("="*40)
-    print("S2")
-    for _s in sorted(list(s2)):
-        print(_s)
+    if arguments['--verbose']:
+        print("S1")
+        for _s in sorted(list(s1)):
+            print(_s)
+        print("="*40)
 
-    print("="*40)
-    print("S")
-    for _s in sorted(list(s)):
-        print(_s)
-    print("="*40)
+        print("S2")
+        for _s in sorted(list(s2)):
+            print(_s)
+        print("="*40)
+
+        print("S")
+        for _s in sorted(list(s)):
+            print(_s)
+        print("="*40)
+
     print("Fu")
     print(format_fact(sorted(list(s))))
 
